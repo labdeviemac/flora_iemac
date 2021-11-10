7375462b7a5fd58cb4fdd0085a02b20e4e735f31
andreadelucca <projetodigitalam@outlook.com>
2021-11-09 20:40:44 -0400

    Adicionadas novas funcionalidades

--- services/Especies.py
+++ services/Especies.py
@@ -30,8 +30,76 @@ class Especie(Conexao):
             data_json = {}
             for data in result:
                 data_json.update({
-                    # DESAFIO
+                    data[0]: {
+                        "especie": data[1],
+                        "tipoadubo": data[2],
+                        "tempovida": data[3],
+                        "intervaloagua": data[4],
+                        "observacoes": data[5],
+                        "fornecedor_id": data[6],
+                        "razaosocial": data[8],
+                        "nomefantasia": data[9],
+                        "cnpj": data[10],
+                        "telefonefornecedor": data[11]
+                    }
                 })
             return json.dumps(data_json)
+        except Exception as e:
+            return e
+
+    def listarEspeciePorId(self, id):
+        cursor = self.conexao.cursor()
+        try:
+            query_sql = f"SELECT * FROM especie as e JOIN fornecedor as f on f.id = e.fornecedor WHERE e.id = {id}"
+            cursor.execute(query_sql)
+            result = cursor.fetchall()
+
+            if len(result) == 0:
+                return 0
+
+            data_json = {}
+            for data in result:
+                data_json.update({
+                        "especieid": data[0],
+                        "especie": data[1],
+                        "tipoadubo": data[2],
+                        "tempovida": data[3],
+                        "intervaloagua": data[4],
+                        "observacoes": data[5],
+                        "fornecedor_id": data[6],
+                        "razaosocial": data[8],
+                        "nomefantasia": data[9],
+                        "cnpj": data[10],
+                        "telefonefornecedor": data[11]
+                })
+            return json.dumps(data_json)
+        except Exception as e:
+            return e
+
+    def atualizarEspecie(self, dados, id):
+        cursor = self.conexao.cursor()
+        try:
+            query_sql = f"UPDATE especie SET {dados} WHERE id = {id}"
+            cursor.execute(query_sql)
+            self.conexao.commit()
+
+            if cursor.rowcount < 1:
+                return 0
+
+            return cursor.rowcount
+        except Exception as e:
+            return e
+
+    def deletarEspecie(self, id):
+        cursor = self.conexao.cursor()
+        try:
+            query_sql = f"DELETE FROM especie WHERE id = {id}"
+            cursor.execute(query_sql)
+            self.conexao.commit()
+
+            if cursor.rowcount < 1:
+                return 0
+
+            return cursor.rowcount
         except Exception as e:
             return e
---
+++ services/Flores.py
@@ -0,0 +1,17 @@
+from services.Conexao import Conexao
+import json
+
+
+class Flores(Conexao):
+
+    def inserirFlores(self, dados):
+        pass
+
+    def listarFlores(self):
+        pass
+
+    def listarFloresPorId(self, id):
+        pass
+
+    def atualizarFlores(self, dados, id):
+        pass
